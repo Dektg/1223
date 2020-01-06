21c86d1cb0d0bfb39d2f77b7d556880cdff7ecec
Dektg <56469979+Dektg@users.noreply.github.com>
2020-01-06 14:46:06 +1000

    Закончил часть кода с отрисовкий поля

--- PinPong2d.py
+++ PinPong2d.py
@@ -2,17 +2,40 @@ import turtle
 
 window = turtle.Screen()
 window.title("2d Ping-Pong")
-window.setup(width=1.0,height=1.0)# Размеры окна на весь монитор
-window.bgcolor("black")# Задний фон
+# Размеры окна на весь монитор
+window.setup(width=1.0,height=1.0)
+# Задний фон
+window.bgcolor("black")
 
+# Отрисовка поля
 border_rectangle = turtle.Turtle()
 border_rectangle.color("white")
+border_rectangle.speed(0)
 border_rectangle.up()
 border_rectangle.goto(-500, 300)
 border_rectangle.down()
+border_rectangle.begin_fill()
 border_rectangle.goto(500, 300)
 border_rectangle.goto(500, -300)
 border_rectangle.goto(-500, -300)
 border_rectangle.goto(-500, 300)
+border_rectangle.end_fill()
+
+border_rectangle.goto(0, 300)
+border_rectangle.color("black")
+# Повернуться вниз
+border_rectangle.setheading(270)
+
+
+# Цикл для пунктира
+for i in range(0,25,1):
+    if i % 2 == 0:
+        border_rectangle.forward(24)
+    else:
+        border_rectangle.up()
+        border_rectangle.forward(24)
+        border_rectangle.down()
+
+border_rectangle.hideturtle()
 
 window.mainloop()
