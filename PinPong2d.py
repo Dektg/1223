150df26d6b1e13e5cd4b0fe3b8ed74153425be0a
Dektg <56469979+Dektg@users.noreply.github.com>
2020-01-06 19:07:52 +1000

    Управление ракетками
    Получилось сделать управление только для одной ракетки

--- PinPong2d.py
+++ PinPong2d.py
@@ -3,7 +3,7 @@ import turtle
 window = turtle.Screen()
 window.title("2d Ping-Pong")
 # Размеры окна на весь монитор
-window.setup(width=1.0,height=1.0)
+window.setup(width=1.0, height=1.0)
 # Задний фон
 window.bgcolor("black")
 
@@ -28,9 +28,8 @@ border_rectangle.color("black")
 # Повернуться вниз
 border_rectangle.setheading(270)
 
-
 # Цикл для пунктира
-for i in range(0,25,1):
+for i in range(0, 25, 1):
     if i % 2 == 0:
         border_rectangle.forward(24)
     else:
@@ -38,5 +37,60 @@ for i in range(0,25,1):
         border_rectangle.forward(24)
         border_rectangle.down()
 
+rocket_a = turtle.Turtle()
+rocket_a.color("black")
+rocket_a.shape("square")
+rocket_a.shapesize(stretch_len=1, stretch_wid=5)
+# Не оставляет следов
+rocket_a.penup()
+rocket_a.goto(-450, 0)
+
+
+def move_up_a():
+    # Узнаю Y у ракетки
+    y_rocket_a = rocket_a.ycor() + 80
+    if y_rocket_a > 250:
+        y_rocket_a = 250
+    rocket_a.sety(y_rocket_a)
+
+
+def move_down_a():
+    # Узнаю Y у ракетки
+    y_rocket_a = rocket_a.ycor() - 80
+    if y_rocket_a < -250:
+        y_rocket_a = -250
+    rocket_a.sety(y_rocket_a)
+
+
+rocket_b = turtle.Turtle()
+rocket_b.color("black")
+rocket_b.shape("square")
+rocket_b.shapesize(stretch_len=1, stretch_wid=5)
+# Не оставляет следов
+rocket_b.penup()
+rocket_b.goto(450, 0)
+
+
+def move_up_b():
+    # Узнаю Y у ракетки
+    y_rocket_b = rocket_b.ycor() + 80
+    if y_rocket_b > 250:
+        y_rocket_b = 250
+    rocket_b.sety(y_rocket_b)
+
+
+def move_down_b():
+    # Узнаю Y у ракетки
+    y_rocket_b = rocket_b.ycor() - 80
+    if y_rocket_b < -250:
+        y_rocket_b = -250
+    rocket_a.sety(y_rocket_b)
+
+
+window.listen()
+window.onkeypress(move_up_a, "q")
+window.onkeypress(move_down_a, "a")
+#window.onkeypress(move_up_b, "w")
+#window.onkeypress(move_down_b, "s")
 
 window.mainloop()
